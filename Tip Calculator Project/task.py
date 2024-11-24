342f7661be87d274a090d3a9cd7f5c9368ba12a6
Alishba-Sajid <alishbasajid121@gmail.com>
2024-11-24 15:22:53 +0800

    Initial commit

---
+++ Tip Calculator Project/task.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? PKR :"))
+tip = int(input("What percentage tip would you like to give? 10 12 15:"))
+people = int(input("How many people to split the bill? "))
+
+Bill_with_tip= tip /100 * bill + bill
+Bill_PerPerson = Bill_with_tip / people
+Final_amount=round(Bill_PerPerson , 2)
+
+print(f"Each person should pay PKR :{Final_amount}")
+
